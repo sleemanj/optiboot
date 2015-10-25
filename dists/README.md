90542e9dc90c0ededc472a0c980d6c86f018aaca
James Sleeman <james@gogo.co.nz>
2015-10-25 16:39:10 +1300

    Add more documentation, restructure zips.

---
+++ dists/README.md
@@ -0,0 +1,86 @@
+DIY AVR in the Arduino for both 1.6.x and 1.0.x IDE
+--------------------------------------------------------------------------------
+
+Included here are a large set of convenient boards.txt and bootloaders to handle
+many Atmel AVR Microcontrollers in the Arduino IDE for people making their own
+DIY Arduino based stuff.
+
+Installing these is a breeze, especially in IDE 1.6.x, but also in 1.0.x (1.0.5 
+tested)
+
+Presently the list of included chips is as follows:
+
+## ATmega8 Series
+
+This series of chips are pin compatible in both 28 pin DIP (through hole) and
+32 pin surface mount packages.  Consult the datasheets for the specific 
+differences, but for most people the biggest difference is simply the amount
+of memory.  
+
+ * ATmega8,   ATmega8A,
+ * ATmega88,  ATmega88A,  ATmega88P,  ATmega88PA, ATmega88PB 
+ * ATmega168, ATmega168A, ATmega168P, ATmega168PA, ATmega168PB
+ * ATmega328, ATmega328P, ATmega328PB
+ * ATmega48,  ATmega48A,  ATmega48P, ATmega48PA, ATmega48PB
+ 
+Note at current time only 8A, 48P and 328P have been tested, but the others 
+should work too, open a support ticket if not.
+
+Installation & Usage
+--------------------------------------------------------------------------------
+
+## IDE Version 1.6.x
+
+Note that this has been tested with 1.6.5, older versions your milage may vary.
+
+### Automatic Installation through Board Manager
+
+ * Start the Arduino IDE
+ * Select File > Preferences
+ * Locate the "Additional Boards Manager URLs" setting and click the button next to it.
+ * Add the following URL to the list
+ * https://raw.githubusercontent.com/sleemanj/optiboot/master/dists/package_gogo_diy_atmega8_series_index.json
+ * Hit OK
+ * Select Tools > Board > Boards Manager
+ * After a few seconds you will find that the new boards are available for 
+   install, select the new Boards and click the Install button
+ * Close the Boards Manager window
+ 
+### Manual Installation
+
+  * Download the following zip file
+  * https://raw.githubusercontent.com/sleemanj/optiboot/master/dists/diy_atmega8_series_ARDUINO_1_6_x_MANUAL_INSTALL.zip
+  * Unzip the file
+  * Move the folder you unzipped into a folder called "hardware" inside your Arduino Sketchbook (create one if it doesn't exist) - see the README.TXT in the file for full instructions on where to find your sketchbook folder
+  * Restart your Arduino IDE
+ 
+### Usage
+ 
+ * Select Tools > Board > [the chip you want to program]
+ * Select Tools > Processor Version > [the specific version of the chip]
+ * Select Tools > Processor Speed > [the speed to run the chip at]
+ * Select Tools > Bootloader > [ if you want/have a bootloader or not ]
+ 
+If you need to burn a bootloader (if it hasn't already been done on your chip or you want to change the chip settings) or set fuses, you can then use the "Burn Bootloader" option to do so, if you selected to use a non-bootloaded setup, then Burn Bootloader will set the appropriate fuses for that too, so make sure you still do it!
+
+## IDE Version 1.0.x
+
+Note that this has been tested with 1.0.5, older versions your milage may vary.
+
+### Manual Installation
+
+  * Download the following zip file
+  * https://raw.githubusercontent.com/sleemanj/optiboot/master/dists/diy_atmega8_series_ARDUINO_1_0_x_MANUAL_INSTALL.zip
+  * Unzip the file
+  * Move the folder you unzipped into a folder called "hardware" inside your Arduino Sketchbook (create one if it doesn't exist) - see the README.TXT in the file for full instructions on where to find your sketchbook folder
+  * Restart your Arduino IDE
+
+See the README.TXT file in the zip file for full install instructions.
+  
+### Usage
+
+  * Select Tools > Board > [the chip, version, speed and bootloader settings you want]
+  
+If you need to burn a bootloader (if it hasn't already been done on your chip or you want to change the chip settings) or set fuses, you can then use the "Burn Bootloader" option to do so, if you selected to use a non-bootloaded setup, then Burn Bootloader will set the appropriate fuses for that too, so make sure you still do it!
+
+Due to limitations of the 1.0.x IDE, only the most common chips have been enabled in boards.txt, if the chip you want is not found in the Board menu, you will need to edit the boards.txt you downloaded in the zip file, locate the section for the chip you want, and uncomment it.  
--- makedist.sh
+++ makedist.sh
@@ -41,23 +41,123 @@ function package_for
   make clean 
   popd
 
+  # Make a "manual install" package suitable for sketchbook/hardware/[whatever]
+  #  in Arduino 1.6
+  pushd dists
+  echo "Arduino 1.6.x boards.txt - MANUAL INSTALLATION PACKAGE
+--------------------------------------------------------------------------------
+
+This file contains manual installation files for boards.txt and optiboot
+bootloaders for various microcontrollers for the 1.6.x Ardiuno IDE.
+
+Arduino IDE 1.6.x allows you to use a boards manager to automatically install
+boards such as these and this is recommended, instead of using this zip file.
+
+See the following URL for the automatic installation instructions
+
+  http://github.com/sleemanj/optiboot/dists/README.md
+
+If you wish to install manually, copy the entire {$PACKAGENAME} folder into your Arduino 
+Sketchbook \"hardware\" sub folder.
+
+You can find what your sketchbook folder is by opening the Preferences in the 
+Arduino IDE and you will see the \"sketchbook location\", commonly the sketchbook
+folder for 1.6.x is...
+
+  * In Windows \`Documents\Arduino\`
+  * In Mac  \`Documents/Arduino\`
+  * In Linux \`$HOME/Arduino\`
+
+So for example, if your sketchbook folder is \`Documents\Arduino\` you would copy
+the {$PACKAGENAME} folder to become \`Documents\Arduino\hardware\{$PACKAGENAME}\`
+
+After installation, restart your Arduino IDE and you can then select the 
+  
+  * The family of chip you want to use (eg ATmega328), from the Boards menu
+  * Processor Version (eg ATmega328p)- after choosing the board, this will appear in a menu
+     underneath the Boards menu.
+  * Processor Speed (eg 8MHz) - after choosing the board, this will appear in a menu
+     underneath the Boards menu.
+  * Bootloader (Yes or No) - after choosing the board, this will appear in a menu
+     underneat the Boards menu.
+     
+" >>$PACKAGENAME/README.TXT
+  zip -r ${PACKAGENAME}_ARDUINO_1_6_x_MANUAL_INSTALL.zip $PACKAGENAME
+  rm $PACKAGENAME/README.TXT
+  popd 
+  
   pushd dists/$PACKAGENAME
   cp -rp avr $PACKAGENAME
-  zip -r $PACKAGENAME.zip $PACKAGENAME   
-  mv $PACKAGENAME.zip ../$PACKAGENAME.zip
+    echo "Arduino 1.6.x boards.txt - AUTOMATIC INSTALLATION PACKAGE
+--------------------------------------------------------------------------------
+
+This file contains manual installation files for boards.txt and optiboot
+bootloaders for various microcontrollers for the 1.6.x Ardiuno IDE.
+
+Arduino IDE 1.6.x allows you to use a boards manager to automatically install
+boards such as these, this file is used by the Boards Manager
+
+See the following URL for the automatic installation instructions
+
+  http://github.com/sleemanj/optiboot/dists/README.md
+     
+" >>$PACKAGENAME/README.TXT
+  zip -r ${PACKAGENAME}_ARDUINO_1_6_x_BOARDS_MANAGER.zip $PACKAGENAME   
+  rm $PACKAGENAME/README.TXT
+  mv ${PACKAGENAME}_ARDUINO_1_6_x_BOARDS_MANAGER.zip ../
   
   # That makes the 1.6.x package now we create a boards.txt for 1.0.x
   #  except we comment out all the boards  
   #  and then uncomment the ones we want
   
-  #$REPOROOT/1.6-to-1.0.sh "avr/boards.txt" | sed "s/^\([a-z0-9_\.]\)/# \1/" | sed "/^# \(.*\\.\($(echo "$IDE10X_LIMIT_TO" | sed "s/ /|/")\)\)/\1/" >$PACKAGENAME/boards.txt
-  local LOOKING_FOR="(_(($(echo "$IDE10X_LIMIT_TO" | sed "s/ /)|(/g")))_)"
-  echo "s/^# \(.*\.${LOOKING_FOR}_[0-9]+MHz\)/\1/"
+  local LOOKING_FOR="(_(($(echo "$IDE10X_LIMIT_TO" | sed "s/ /)|(/g")))_.*\.)"  
   $REPOROOT/1.6-to-1.0.sh "avr/boards.txt" | sed "s/^\([a-z0-9_\.]\)/# \1/" \
     | sed -r "s/^# (.*${LOOKING_FOR}.*)$/\1/" \
     >$PACKAGENAME/boards.txt
   
-  #rm -r $PACKAGENAME
+  # 1.0.x doesn't need platform.txt
+  rm $PACKAGENAME/platform.txt
+  
+  # A readme will be necessary
+  echo "Arduino 1.0.x boards.txt
+--------------------------------------------------------------------------------
+
+This file contains boards.txt and optiboot bootloaders for various 
+microcontrollers and is made for the 1.0.x IDE (or rather, 1.0.5 or later, I
+have not tested in older).
+
+To install this, copy the entire {$PACKAGENAME} folder into your Arduino 
+Sketchbook \"hardware\" sub folder.
+
+You can find what your sketchbook folder is by opening the Preferences in the 
+Arduino IDE and you will see the \"sketchbook location\", commonly the sketchbook
+folder is...
+
+  * In Windows \`Documents\Arduino\`
+  * In Mac  \`Documents/Arduino\`
+  * In Linux \`$HOME/sketchbook\`
+
+So for example, if your sketchbook folder is \`Documents\Arduino\` you would copy
+the {$PACKAGENAME} folder to become \`Documents\Arduino\hardware\{$PACKAGENAME}\`
+
+When you have copied the folder, restart the Arduino IDE and you should be able
+to choose from new items in the Boards menu.  Note that configurations have been
+made for chips both with and without bootloader, the \"Upload Bootloader\"
+function for those without bootloaders just erases the chip and sets appropriate 
+fuses.
+
+Due to limitations of the Arduino 1.0.x IDE, there are likely many chips in
+the boards.txt which have been disabled in order to not have your Boards menu
+too long.  The most common chips have been left enabled.
+
+So if you find your particular chip is not present in the Boards 
+menu, open {$PACKAGENAME}/boards.txt and search for it, then you can uncomment
+(remove the # marks) from the settings for that board to enable it.
+" >$PACKAGENAME/README.TXT
+  
+  zip -r "${PACKAGENAME}_ARDUINO_1_0_x.zip" $PACKAGENAME   
+  mv  "${PACKAGENAME}_ARDUINO_1_0_x.zip" ../
+  rm -r $PACKAGENAME
   popd 
   
   pushd dists
